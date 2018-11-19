23f0662c270b37bbcd8ce9deef52dca78b59f1f5
Fab Nan <notanumber@hotmail.it>
2018-11-19 08:53:51 +0100

    .

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import CardList from './cardList';
+import * as serviceWorker from './serviceWorker';
+import 'tachyons';
+import {robots} from './robots';
+import App from './App';
+
+ReactDOM.render(
+                <App />
+
+, document.getElementById('root'));
---
+++ src/robots.js
@@ -0,0 +1,63 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Utente1',
+    username: 'aaaa',
+    email: 'aaaa@gmail.com'
+  },
+  {
+    id: 2,
+    name: 'Utente2',
+    username: 'bbbb',
+    email: 'bbbb@gmail.com'
+  },
+  {
+    id: 3,
+    name: 'Utente3',
+    username: 'cccc',
+    email: 'cccc@gmail.com'
+  },
+  {
+    id: 4,
+    name: 'Utente4',
+    username: 'dddd',
+    email: 'dddd@gmail.com'
+  },
+  {
+    id: 5,
+    name: 'Utente5',
+    username: 'eeee',
+    email: 'eeee@gmail.com'
+  },
+  {
+    id: 6,
+    name: 'Utente6',
+    username: 'ffff',
+    email:'ffff@gmail.com'
+  },
+  {
+    id: 7,
+    name: 'Utente7',
+    username: 'gggg',
+    email: 'gggg@gmail.com'
+  },
+  {
+    id: 8,
+    name:'Utente8',
+    username: 'hhhh',
+    email:'hhhh@gmail.com'
+  },
+  {
+    id: 9,
+    name: 'Utente9',
+    username: 'iiii',
+    email: 'iiii@gmail.com'
+  },
+  {
+    id: 10,
+    name:'Utente10',
+    username: 'llll',
+    email:'llll@gmail.com'
+  },
+  
+];
